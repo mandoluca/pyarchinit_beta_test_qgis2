7724bf35480c39c5d39c5df6fffba62efb7e6d21
mandoluca <mandoluca@gmail.com>
2013-07-09 01:03:26 +0200

    Update test_repo.py

--- test_repo.py
+++ test_repo.py
@@ -3,5 +3,5 @@ print sys.path
 <<<<<<< HEAD
 print "hallo" #aggiunto per il test_branch
 =======
-print "hallo world" #aggiunto per il test_branch
+print "hallo world gennaro" #aggiunto per il test_branch
 >>>>>>> origin/test_branch
